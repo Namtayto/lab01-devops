253142d8d520fa9f22ad387f132723a24679dd20
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-10-13 14:28:10 +0700

    Create Route Table for Public and Private subnet
    Co-Authored-By: huyf-51 <139951521+huyf-51@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -15,3 +15,10 @@ module "security_group" {
   source = "./security_group"
   vpc_id = module.vpc.vpc_id
 }
+
+module "route_table" {
+  source = "./route_table"
+  vpc_id = module.vpc.vpc_id
+  public_subnet_id = module.subnets.public_subnet_id
+  private_subnet_id = module.subnets.private_subnet_id
+}
